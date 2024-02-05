1337d1a8a637bc0372def494836d91b9cb5a126f
DS0708 <tlsehdtjd070@naver.com>
2024-02-05 11:45:26 +0900

    현재 시간 따오기

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [startTime, setStartTime] = useState("");
+
+  useEffect(() => {
+    let sampleTimestamp = Date.now(); //현재시간 타임스탬프 13자리 예)1599891939914
+    let date = new Date(sampleTimestamp); //타임스탬프를 인자로 받아 Date 객체 생성 
+    console.log(date);
+    var year = date.getFullYear().toString().slice(-4); //년도 뒤에 4자리
+    var month = ("0" + (date.getMonth() + 1)).slice(-2); //월 2자리 (01, 02 ... 12)
+    var day = ("0" + date.getDate()).slice(-2); //일 2자리 (01, 02 ... 31)
+    var hour = ("0" + date.getHours()).slice(-2); //시 2자리 (00, 01 ... 23)
+    var minute = ("0" + date.getMinutes()).slice(-2); //분 2자리 (00, 01 ... 59)
+    var second = ("0" + date.getSeconds()).slice(-2); //초 2자리 (00, 01 ... 59)
+    setStartTime(year);
+  
+  }, [])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="timer">
+      <div>Hello timer</div>
+      <div className="startTime">{startTime}</div>
     </div>
+    
   );
 }
 
